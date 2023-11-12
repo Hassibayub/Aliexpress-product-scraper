6f22176ebb256609b85a277560aacb87b2d2484c
Muhammad Haseeb <miltech.haseeb@gmail.com>
2023-11-12 13:20:06 +0500

    Download images from description

--- main.py
+++ main.py
@@ -5,14 +5,17 @@ import os
 
 
 def main():
-    product_id = "1005006201952621"
-    result = AliexpressProductScraper(product_id, 10)
+    product_ids = ["1005006201952621"]
+    
+    for product_id in product_ids:
+        result = AliexpressProductScraper(product_id, 10)
 
-    download_images(result["images"], product_id)
+        download_images(result["images"], product_id)
+        download_images(result["description"], product_id, is_desc=True)
 
-    json_path = os.path.join("outputs", product_id, f"{product_id}.json")
-    with open(json_path, "w") as f:
-        json.dump(result, f, indent=4)
+        json_path = os.path.join("outputs", product_id, f"{product_id}.json")
+        with open(json_path, "w") as f:
+            json.dump(result, f, indent=4)
 
 
 if __name__ == "__main__":
--- src/downloader.py
+++ src/downloader.py
@@ -1,9 +1,19 @@
 import requests
 import os
+import re
 
-
-def download_images(images, product_id):
-    for i, image in enumerate(images):
-        image_path = os.path.join("outputs", product_id, f"{product_id}_{i}.jpg")
-        with open(image_path, "wb") as f:
-            f.write(requests.get(image).content)
+def download_images(images, product_id, is_desc=False):
+    
+    if is_desc:
+        results = re.findall(r'"https:.+?alicdn.+?"', images)
+        if results:
+            for idx, result in enumerate(results):
+                image_path = os.path.join("outputs", product_id, f"desc_{idx}.jpg")
+                with open(image_path, "wb") as f:
+                    f.write(requests.get(result[1:-1]).content)
+        
+    else:        
+        for i, image in enumerate(images):
+            image_path = os.path.join("outputs", product_id, f"{product_id}_{i}.jpg")
+            with open(image_path, "wb") as f:
+                f.write(requests.get(image).content)
