4a5aa090e8d4feae534700a807174a2db04e4cc1
Muhammad Haseeb <miltech.haseeb@gmail.com>
2023-11-12 12:41:39 +0500

    main.py download and save json into respective dir

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from src.ali_express import AliexpressProductScraper
-
+from src.downloader import download_images
+import json
+import os
 
 def main():
-    result = AliexpressProductScraper('1005006201952621', 10)
-    print(result)
+    product_id = '1005006201952621'
+    result = AliexpressProductScraper(product_id, 10)
+    
+    download_images(result['images'], product_id)
+    
+    json_path = os.path.join('outputs', product_id, f'{product_id}.json')
+    with open(json_path, 'w') as f:
+        json.dump(result, f, indent=4)
 
 
 if __name__ == '__main__':
